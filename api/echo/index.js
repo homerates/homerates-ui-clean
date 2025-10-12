c4563ee3d5241d1246ef24993a7f0224220c6568
Rayaan Arif <legatum2005@gmail.com>
2025-10-12 16:11:14 -0700

    chore: lock routes to /api/echo only + add /ping.txt

--- api/echo/index.js
+++ api/echo/index.js
@@ -2,5 +2,5 @@
   const sha = process.env.VERCEL_GIT_COMMIT_SHA || "local";
   res.statusCode = 200;
   res.setHeader("content-type", "application/json");
-  res.end(JSON.stringify({ ok:true, sha, path:req.url, ts: Date.now() }));
+  res.end(JSON.stringify({ ok: true, sha, path: req.url, ts: Date.now() }));
 };
